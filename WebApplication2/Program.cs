b9eccaf3cbb2bdaec0696aeaa4cc77824fe99309
abeni <abeni@email.com>
2025-09-09 06:39:50 -0700

    enter list names

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -21,10 +21,47 @@ app.Run(async (HttpContext context) =>
         }
         else if (context.Request.Path.StartsWithSegments("/employees"))
         {
-            await context.Response.WriteAsync("Employee List");
+            //  await context.Response.WriteAsync("Employee List");
+            var employees = EmployeeRepository.GetEmployees();
+
+            foreach(var employee in employees) {
+                await context.Response.WriteAsync($"{employee.Name}: {employee.Position}\r\n");
+            }
         }
     }
     
 });
 
 app.Run();
+
+static class EmployeeRepository
+{
+    private static List<Employee> employees = new List<Employee>
+    {
+        new Employee(1, "Abeni Sileshi" , "Programmer" , 60000),
+        new Employee(2, "Tebeyen Bekele", " Security", 70000),
+        new Employee(3, "Hasan Ahmed", "Cyber" , 100000)
+    };
+
+    public static List<Employee> GetEmployees() => employees;
+}
+
+public class Employee
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public string Position { get; set; }
+
+    public double Salary { get; set; }
+
+    public Employee(int id, string name, string position, double salary)
+    {
+        Id = id;
+        Name = name;
+        Position = position;
+        Salary = salary;
+
+    }
+}
