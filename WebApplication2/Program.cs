4eb188158377d905a67afe8b5040608d84f63ab2
abeni <abeni@email.com>
2025-09-09 05:18:17 -0700

    Employee List

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -4,17 +4,27 @@ var app = builder.Build();
 app.Run(async (HttpContext context) =>
 {
     // context.Request
-
-    await context.Response.WriteAsync("The method is: {context.Request.Method}");
-    await context.Response.WriteAsync("The Url is : {context.");
-
-    await context.Response.WriteAsync($"\r\nHeaders:r\n");
-
-    foreach(var key in context.Request.Headers.Keys)
+    if (context.Request.Method == "GET")
     {
-        await context.Response.WriteAsync($"{key}:{ context.Request.Headers[key]}\r\n");
+        if (context.Request.Path.StartsWithSegments("/"))
+        {
+            await context.Response.WriteAsync("The method is: {context.Request.Method}");
+            await context.Response.WriteAsync("The Url iss : {context.");
 
+            await context.Response.WriteAsync($"\r\nHeaders:r\n");
+
+            foreach (var key in context.Request.Headers.Keys)
+            {
+                await context.Response.WriteAsync($"{key}:{context.Request.Headers[key]}\r\n");
+
+            }
+        }
+        else if (context.Request.Path.StartsWithSegments("/employees"))
+        {
+            await context.Response.WriteAsync("Employees List");
+        }
     }
+    
 });
 
 app.Run();
